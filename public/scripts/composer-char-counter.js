9d95a984bc89ef925184e309f65bfb7886b1e70e
Juliet Li <li_juliet@hotmail.com>
2021-08-10 19:04:45 +0000

    add composer-char-counter.js to decrease the characters for each tweet, changed width size for new-tweet.css

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('ready');
+ $('#tweet-text').on('input', function() {
+   let currentLength = $(this).val().length;
+   let counter = $(this).parent().next(['button-and-counter']).find('.counter');
+   counter.text(140 - currentLength);
+   if (counter.text() < 0) {
+    counter.css("color", "red")
+   }
+ })
+});
+
+
+
