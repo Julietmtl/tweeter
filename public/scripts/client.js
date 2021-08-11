7b854babb22c44d6691ea12849ea975e3fce099a
Juliet Li <li_juliet@hotmail.com>
2021-08-11 19:58:25 +0000

    use data to create tweets in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const tweetData = {
   "user": {
@@ -18,10 +43,20 @@ const tweetData = {
 
 $(document).ready(function () {
 
+  const renderTweets = function (tweets) {
+    // loops through tweets
+
+    for (let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet)
+    }
+  }
 
   const createTweetElement = function (data) {
     const time = timeago.format(data.created_at)
-    const html =
+    const $tweet =
 
       `<header>
       <span><img src='${data.user.avatars}'>${data.user.name} </span>
@@ -36,12 +71,15 @@ $(document).ready(function () {
                 <span><i class="fas fa-heart"></i></span>
               </div>
             </footer>`
-    return html
+    return $tweet
   }
-  const $tweet = createTweetElement(tweetData);
 
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
+
+  // console.log($tweet);
+  // $('#tweets-container').append($tweet);
+
+
 
 });
 
