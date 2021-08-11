a96acb4757e8e857ad89f7156f6240f1acac63e6
Juliet Li <li_juliet@hotmail.com>
2021-08-11 18:31:22 +0000

    function createTweetElement, add timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,47 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+$(document).ready(function () {
+
+
+  const createTweetElement = function (data) {
+    const time = timeago.format(data.created_at)
+    const html =
+
+      `<header>
+      <span><img src='${data.user.avatars}'>${data.user.name} </span>
+            <span class="username">${data.user.handle}</span>
+        </header>
+            <p class="tweet-post">${data.content.text}</p>
+            <footer>
+              <span class="days-ago"> ${time} </span>
+              <div class="tweet-icons">
+                <span><i class="fas fa-flag"></i></span>
+                <span><i class="fas fa-retweet"></i></span>
+                <span><i class="fas fa-heart"></i></span>
+              </div>
+            </footer>`
+    return html
+  }
+  const $tweet = createTweetElement(tweetData);
+
+  console.log($tweet);
+  $('#tweets-container').append($tweet);
+
+});
+
+
+
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log('ready');
- $('#tweet-text').on('input', function() {
-   let currentLength = $(this).val().length;
-   let counter = $(this).parent().next(['button-and-counter']).find('.counter');
-   counter.text(140 - currentLength);
-   if (counter.text() < 0) {
-    counter.addClass('counter-negative');
-   } else {
-     counter.removeClass('counter-negative');
-   }
- })
+  $('#tweet-text').on('input', function () {
+    let currentLength = $(this).val().length;
+    let counter = $(this).parent().next(['button-and-counter']).find('.counter');
+    counter.text(140 - currentLength);
+    if (counter.text() < 0) {
+      counter.addClass('counter-negative');
+    } else {
+      counter.removeClass('counter-negative');
+    }
+  })
 });
 
 
